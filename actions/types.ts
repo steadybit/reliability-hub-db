44af8b41cf5b0717394672343378efd8872a8a9d
Daniel Reuter <daniel@steadybit.com>
2024-10-02 12:04:56 +0200

    feat: add tags to action descriptions

--- actions/types.ts
+++ actions/types.ts
@@ -2,39 +2,40 @@
  * Copyright 2023 steadybit GmbH. All rights reserved.
  */
 
-import {ExtensionId} from "../extensions/types";
+import { ExtensionId } from "../extensions/types";
 
 export type Deprecation = ReplacementDeprecation | FullDeprecation;
 
 export interface ReplacementDeprecation {
-    type: "replacement";
-    newActionId: string;
+  type: "replacement";
+  newActionId: string;
 }
 
 export interface FullDeprecation {
-    type: "deprecated";
+  type: "deprecated";
 }
 
 export function isReplacementDeprecation(
-    d: Deprecation,
+  d: Deprecation,
 ): d is ReplacementDeprecation {
-    return d.type === "replacement";
+  return d.type === "replacement";
 }
 
 export function isFullDeprecation(d: Deprecation): d is FullDeprecation {
-    return d.type === "deprecated";
+  return d.type === "deprecated";
 }
 
 export interface ActionDescription {
-    id: string;
-    label: string;
-    icon: string;
-    kind: 'attack' | 'check' | 'load_test' | 'other';
-    category: string | null;
-    targetType?: string | null;
-    description: string;
-    extension: ExtensionId;
-    deprecation?: Deprecation;
-    promotedActions: string[];
-    releaseDate?: string;
+  id: string;
+  label: string;
+  icon: string;
+  kind: "attack" | "check" | "load_test" | "other";
+  category: string | null;
+  targetType?: string | null;
+  description: string;
+  extension: ExtensionId;
+  deprecation?: Deprecation;
+  promotedActions: string[];
+  releaseDate?: string;
+  tags: string[];
 }
